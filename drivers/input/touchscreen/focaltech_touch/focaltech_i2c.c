bc6563e0beb93375ceb086e284b62ee362a85b28
Aarqw12 <lcockx@protonmail.com>
2020-07-30 18:59:15 +0000

    touchscreen : ft5446 : update to Focaltech V2.2
    *imported from kernel mi-code Focaltech V2.2
    
    *edited for onclite and fixed error & warning compile

--- drivers/input/touchscreen/focaltech_touch/focaltech_i2c.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_i2c.c
@@ -3,7 +3,7 @@
  * FocalTech TouchScreen driver.
  *
  * Copyright (c) 2012-2018, FocalTech Systems, Ltd., all rights reserved.
- * Copyright (C) 2020 XiaoMi, Inc.
+ * Copyright (C) 2019 XiaoMi, Inc.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -188,7 +188,7 @@ int fts_i2c_read_reg(struct i2c_client *client, u8 regaddr, u8 *regvalue)
 /************************************************************************
 * HID to standard I2C
 ***********************************************************************/
-int fts_i2c_hid2std(struct i2c_client *client)
+void fts_i2c_hid2std(struct i2c_client *client)
 {
 	int ret = 0;
 	u8 buf[3] = {0xeb, 0xaa, 0x09};
@@ -208,7 +208,6 @@ int fts_i2c_hid2std(struct i2c_client *client)
 			FTS_ERROR("hidi2c change to stdi2c fail");
 		}
 	}
-	return ret;
 }
 
 /************************************************************************
