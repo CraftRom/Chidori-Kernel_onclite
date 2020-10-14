9889f5d0a1796fffa187170b670b3be58a319e50
Greg Kroah-Hartman <gregkh@google.com>
2020-10-14 12:02:55 +0200

    Merge 4.9.239 into android-4.9-q
    Changes in 4.9.239
    	vsock/virtio: use RCU to avoid use-after-free on the_virtio_vsock
    	vsock/virtio: stop workers during the .remove()
    	USB: gadget: f_ncm: Fix NDP16 datagram validation
    	gpio: tc35894: fix up tc35894 interrupt configuration
    	Input: i8042 - add nopnp quirk for Acer Aspire 5 A515
    	drm/amdgpu: restore proper ref count in amdgpu_display_crtc_set_config
    	net: dec: de2104x: Increase receive ring size for Tulip
    	rndis_host: increase sleep time in the query-response loop
    	drivers/net/wan/lapbether: Make skb->protocol consistent with the header
    	drivers/net/wan/hdlc: Set skb->protocol before transmitting
    	mac80211: do not allow bigger VHT MPDUs than the hardware supports
    	nfs: Fix security label length not being reset
    	clk: samsung: exynos4: mark 'chipid' clock as CLK_IGNORE_UNUSED
    	iommu/exynos: add missing put_device() call in exynos_iommu_of_xlate()
    	i2c: cpm: Fix i2c_ram structure
    	random32: Restore __latent_entropy attribute on net_rand_state
    	net/packet: fix overflow in tpacket_rcv
    	epoll: do not insert into poll queues until all sanity checks are done
    	epoll: replace ->visited/visited_list with generation count
    	epoll: EPOLL_CTL_ADD: close the race in decision to take fast path
    	ep_create_wakeup_source(): dentry name can change under you...
    	netfilter: ctnetlink: add a range check for l3/l4 protonum
    	fbdev, newport_con: Move FONT_EXTRA_WORDS macros into linux/font.h
    	Fonts: Support FONT_EXTRA_WORDS macros for built-in fonts
    	Revert "ravb: Fixed to be able to unload modules"
    	fbcon: Fix global-out-of-bounds read in fbcon_get_font()
    	net: wireless: nl80211: fix out-of-bounds access in nl80211_del_key()
    	usermodehelper: reset umask to default before executing user process
    	platform/x86: thinkpad_acpi: initialize tp_nvram_state variable
    	platform/x86: thinkpad_acpi: re-initialize ACPI buffer size when reuse
    	driver core: Fix probe_count imbalance in really_probe()
    	perf top: Fix stdio interface input handling with glibc 2.28+
    	mtd: rawnand: sunxi: Fix the probe error path
    	ftrace: Move RCU is watching check after recursion check
    	macsec: avoid use-after-free in macsec_handle_frame()
    	mm/khugepaged: fix filemap page_to_pgoff(page) != offset
    	sctp: fix sctp_auth_init_hmacs() error path
    	team: set dev->needed_headroom in team_setup_by_port()
    	net: team: fix memory leak in __team_options_register
    	openvswitch: handle DNAT tuple collision
    	drm/amdgpu: prevent double kfree ttm->sg
    	xfrm: clone XFRMA_REPLAY_ESN_VAL in xfrm_do_migrate
    	xfrm: clone whole liftime_cur structure in xfrm_do_migrate
    	net: stmmac: removed enabling eee in EEE set callback
    	xfrm: Use correct address family in xfrm_state_find
    	bonding: set dev->needed_headroom in bond_setup_by_slave()
    	mdio: fix mdio-thunder.c dependency & build error
    	rxrpc: Fix rxkad token xdr encoding
    	rxrpc: Downgrade the BUG() for unsupported token type in rxrpc_read()
    	rxrpc: Fix some missing _bh annotations on locking conn->state_lock
    	rxrpc: Fix server keyring leak
    	perf: Fix task_function_call() error handling
    	mm: khugepaged: recalculate min_free_kbytes after memory hotplug as expected by khugepaged
    	net: usb: rtl8150: set random MAC address when set_ethernet_addr() fails
    	Linux 4.9.239
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I425f266783f834eac9a2eaf95bbf7217a7128595

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -65,9 +65,9 @@
 #include <linux/page_owner.h>
 #include <linux/kthread.h>
 #include <linux/memcontrol.h>
+#include <linux/khugepaged.h>
 #include <linux/show_mem_notifier.h>
 #include <linux/psi.h>
-#include <linux/khugepaged.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
