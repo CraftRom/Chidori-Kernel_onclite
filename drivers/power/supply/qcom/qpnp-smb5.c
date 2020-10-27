b4bb615c0bb39beb96ffc4dd4a3a99958057cf8a
Dhina17 <dhinalogu@gmail.com>
2020-10-27 12:07:33 +0530

    Merge tag 'LA.UM.8.6.2.r1-08600-89xx.0' of https://source.codeaurora.org/quic/la/kernel/msm-4.9 into android-10
    "LA.UM.8.6.2.r1-08600-89xx.0"
    
     Conflicts:
    	drivers/power/supply/qcom/qpnp-smb5.c
    
    Change-Id: I869d9588a70bc6548969aa8cf48371eaf7a70f1e

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -1277,8 +1277,9 @@ static enum power_supply_property smb5_batt_props[] = {
 	POWER_SUPPLY_PROP_CYCLE_COUNT,
 	POWER_SUPPLY_PROP_RECHARGE_SOC,
 	POWER_SUPPLY_PROP_CHARGE_FULL,
-	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
         POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
+	//begin for the total capacity of batt in  2018.11.05
+	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
 	#ifdef XIAOMI_CHARGER_RUNIN
 	POWER_SUPPLY_PROP_CHARGING_ENABLED,//lct add for runin 20181105
 	#endif
@@ -1444,10 +1445,6 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_RECHARGE_SOC:
 		val->intval = chg->auto_recharge_soc;
-		break;
-	case POWER_SUPPLY_PROP_CHARGE_FULL:
-		rc = smblib_get_prop_from_bms(chg,
-				POWER_SUPPLY_PROP_CHARGE_FULL, val);
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
 		rc = smblib_get_prop_from_bms(chg,
