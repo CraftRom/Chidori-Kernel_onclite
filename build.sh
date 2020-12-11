317801ddf06355acbdf37762028096d01dca8fef
johnmart19 <johnivan19999@gmail.com>
2020-12-12 00:24:18 +0200

    BuildScript: Compile with All Proton Clang 12 Make options

--- build.sh
+++ build.sh
@@ -9,23 +9,44 @@ KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel3
 CONFIG=onclite-perf_defconfig
-CROSS_COMPILE="aarch64-linux-android-"
-CROSS_COMPILE_ARM32="arm-linux-androideabi-"
-PATH=:"${KERNEL_DIR}/silont-clang/bin:${PATH}:${KERNEL_DIR}/stock/bin:${PATH}:${KERNEL_DIR}/stock_32/bin:${PATH}"
 
 # Export
 export ARCH=arm64
 export CROSS_COMPILE
 export CROSS_COMPILE_ARM32
 
+# Main Staff
+
+clang_bin="$HOME/toolchains/proton-clang/bin"
+gcc_prefix64="aarch64-linux-gnu-"
+gcc_prefix32="arm-linux-gnueabi-"
+CROSS_COMPILE="aarch64-linux-gnu-"
+CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
+
+_ksetup_old_path="$PATH"
+export PATH="$clang_bin:$PATH"
+
 # Build start
-START=$(date +%s)
-make O=out $CONFIG
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-CLANG_TRIPLE=aarch64-linux-gnu- \
-CROSS_COMPILE=aarch64-linux-android-
+make	O=out $CONFIG
+make	\
+	O=out \
+	ARCH=arm64 \
+	CC="ccache clang" \
+	AR=llvm-ar \
+	NM=llvm-nm \
+	LD=ld.lld \
+	STRIP=llvm-strip \
+	OBJCOPY=llvm-objcopy \
+	OBJDUMP=llvm-objdump \
+	OBJSIZE=llvm-size \
+	READELF=llvm-readelf \
+	HOSTCC=clang \
+	HOSTCXX=clang++ \
+	HOSTAR=llvm-ar \
+	HOSTLD=ld.lld \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+	-j`nproc --all`
 
 if ! [ -a $KERN_IMG ]; then
     echo "Build error!"
@@ -40,7 +61,8 @@ cd ..
 # Credit Adek Maulana <adek@techdro.id>
 OUTDIR="$KERNEL_DIR/out/"
 VENDOR_MODULEDIR="$KERNEL_DIR/AnyKernel3/modules/vendor/lib/modules"
-STRIP="$KERNEL_DIR/stock/bin/$(echo "$(find "$KERNEL_DIR/stock/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+
+STRIP="$HOME/toolchains/proton-clang/aarch64-linux-gnu/bin/strip$(echo "$(find "$HOME/toolchains/proton-clang/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
             sed -e 's/gcc/strip/')"
 for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
     "${STRIP}" --strip-unneeded --strip-debug "${MODULES}"
@@ -50,10 +72,12 @@ for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
             "${MODULES}"
     find "${OUTDIR}" -name '*.ko' -exec cp {} "${VENDOR_MODULEDIR}" \;
 done
+cd libufdt/src && python2 mkdtboimg.py create $OUTDIR/arch/arm64/boot/dtbo.img $OUTDIR/arch/arm64/boot/dts/qcom/*.dtbo
 echo -e "\n(i) Done moving modules"
 
 cd $ZIP_DIR
 cp $KERN_IMG zImage
+cp $OUTDIR/arch/arm64/boot/dtbo.img $ZIP_DIR
 make normal &>/dev/null
 echo "Flashable zip generated under $ZIP_DIR."
 cd ..
