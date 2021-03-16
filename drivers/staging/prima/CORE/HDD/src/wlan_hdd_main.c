84b23cf03cb507d36b17037175014fb703621016
Dundi Raviteja <dundi@codeaurora.org>
2021-03-16 20:30:55 +0530

    wlan: Initialize sw_pta before vos_open
    bt_profile_state can be triggered before initializing sw_pta
    which can cause accessing elements that are not initialized.
    
    To address this issue initialize sw_pta before registering
    bt_profile_state.
    
    Change-Id: I3eebf2dfd03658ca89b11e7b0b4a07ba96d6dc82
    CRs-Fixed: 2899060

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
@@ -14814,7 +14814,7 @@ int hdd_wlan_startup(struct device *dev )
 
    mutex_init(&pHddCtx->cache_channel_lock);
 
-   hdd_init_sw_pta(pHddCtx);
+   wcnss_update_bt_profile();
    goto success;
 
 err_open_cesium_nl_sock:
