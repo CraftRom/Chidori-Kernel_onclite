a75123a7825315e9a69f361af2a71f39b4780bad
Sultan Alsawaf <sultan@kerneltoast.com>
2022-04-24 16:56:50 -0700

    rcu: Don't force rcu_normal on Android
    Android works much better with rcu_expedited, since it speeds up a lot of
    procedures that are important to Android such as process death, which is
    needed to free up processes' resources quickly when memory runs low.
    
    Since Android isn't designed to be a real-time userspace anyway, remove the
    rcu_normal restrictions on RT for Android.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [Jprimero15: modified a bit and change the config since we dont use RT]
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- kernel/rcu/update.c
+++ kernel/rcu/update.c
@@ -62,8 +62,11 @@
 #ifndef CONFIG_TINY_RCU
 module_param(rcu_expedited, int, 0);
 module_param(rcu_normal, int, 0);
-static int rcu_normal_after_boot;
+static int rcu_normal_after_boot =
+	IS_ENABLED(CONFIG_ARCH_SDM439) && !IS_ENABLED(CONFIG_ANDROID);
+#if !defined(CONFIG_ARCH_SDM439) || defined(CONFIG_ANDROID)
 module_param(rcu_normal_after_boot, int, 0);
+#endif
 #endif /* #ifndef CONFIG_TINY_RCU */
 
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
